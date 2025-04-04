ba4b76fec8cc7622e6d1fe3c35bc1e44b79f69f3
Beknur <a.beknur@tadi.uz>
2025-04-04 22:01:00 +0500

    Add GitHub Actions workflow for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import MillionLint from "@million/lint";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [MillionLint.vite()],
+  base: process.env.NODE_ENV === 'production' ? '/inomed/' : '/',
   server: {
     port: 5000,
     host: "0.0.0.0"
